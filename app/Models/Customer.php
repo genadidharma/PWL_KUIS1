cd6ba982eebbea078bc2c8e0c13af908ccb661e5
Genadi Dharma <genadislawa@gmail.com>
2021-03-09 18:16:33 +0800

    show Customers created, css fixed

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
     use HasFactory;
+
+    public static function customers(){
+        return Customer::orderBy('name')
+            ->get();
+    }
 }
