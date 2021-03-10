0a9ecee3189045333c8ee0987a48ed8126dc2a29
= <=>
2021-03-10 11:19:06 +0700

    blog seeder fixed

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Blog;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
     public function index(){
-        return view('blog');
+        $blog = Blog::blogs();
+
+        $header = $blog->where('slug', 'the-key-to-creative-work')->first();
+        $side = $blog->filter(function($item){
+            return $item->slug != 'the-key-to-creative-work';
+        });
+        return view('blog',['header' => $header,'side' => $side]);
     }
 
     public function show($slug){
